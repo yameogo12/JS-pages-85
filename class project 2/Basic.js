fad980d0c656202e74ebe2b61844706968163f95
samuel <samuelsyameogo@gmail.com>
2020-02-10 16:54:30 -0800

    y
    c

---
+++ class project 2/Basic.js
@@ -0,0 +1,42 @@
+// var foo = document.getElementsByTagName("p");
+
+function subtraction_Function() {
+	var Subtraction = 5- 2;
+	document.getElementById("Math").innerHTML= "5 - 2 = " + Subtraction;
+};
+//var X = 5;
+//var Y = 2;
+//var Z = X - Y;
+//document.getElementById("Math").innerHTML = Z;
+
+function multiplication_Function() {
+	var simple_Math = 6 * 8;
+	document.getElementById("Math").innerHTML = "6 X 8 = " + simple_Math;
+};
+
+function division_Function() {
+	var simple_Math = 48/ 6;
+	document.getElementById("Math").innerHTML = "48 / 6 = " +simple_Math;
+};
+
+//function math_Function() {
+//	var simple_Math = (1 + 2) * 10 / 2 - 5;
+//	document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10'
+//	divided in half and then subtracted by 5 equals " + simple_Math;
+//};
+
+//function modulus_Operator() {
+//	var simple_Math = 25% 6;
+//	document.getElementById("Math").innerHTML= "When you divide 25 by 6 you have a 
+//	remainder of : " +simple_Math;
+//};
+window.alert(Math.random());
+window.alert(Math.random() * 100);
+
+
+// page 85 Assignment
+
+function Add_Function(){
+	var addition = 75 + 89;
+	document.getElementById("Math").innerHTML ="75+89=" + addition;
+};
